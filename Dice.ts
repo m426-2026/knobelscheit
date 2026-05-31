aba7a87f286788b2fbcc7ed907c1103fa2a5ba2a
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-31 10:50:48 +0200

    Update Dice.ts add DicePair class & implemented roll function
    Co-Authored-By: ZendeRRR <2706293+zenderrr@users.noreply.github.com>

--- Dice.ts
+++ Dice.ts
@@ -3,3 +3,15 @@ export class Dice{
         return Math.floor(Math.random() * 6) + 1;
     }
 }
+
+
+export class DicePair{
+    constructor(
+        private readonly firstDice : Dice = new Dice(),
+        private readonly secondDice : Dice = new Dice()
+    ) {}
+
+    roll(): [number, number] {
+        return [this.firstDice.roll(), this.secondDice.roll()]
+    }
+}
