7885fc6a4e2c16cbeb48010afc38783737e351f5
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-31 10:51:06 +0200

    Update DicePair add DicePair.roll test
    Co-Authored-By: ZendeRRR <2706293+zenderrr@users.noreply.github.com>

--- Dice_test.ts
+++ Dice_test.ts
@@ -1,5 +1,6 @@
 import {assert} from "@std/assert"
 import { Dice } from "./Dice.ts";
+import { DicePair } from "./Dice.ts";
 
 Deno.test("Dice.roll returns a number between 1 and 6", () => {
     const dice = new Dice();
@@ -10,5 +11,20 @@ Deno.test("Dice.roll returns a number between 1 and 6", () => {
 
     assert(value >= 1);
     assert(value <= 6);
+    }
+});
+
+Deno.test("DicePair.roll returns two dice value between 1 and 6", () => {
+
+    const dicePair = new DicePair();
+
+    for( let i = 0; i < 10; i++){
+        const [first, second] = dicePair.roll();
+
+        assert(first >= 1);
+        assert(first <= 6);
+
+        assert(second >= 1);
+        assert(second <= 6);
     }
 });
